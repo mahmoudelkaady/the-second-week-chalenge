4d3b4bae35a6af0bb0c629695ab8a9ca3febad99
mahmoud medhat <mahmodmht.8.8@gmail.com>
2023-04-17 13:14:55 +0200

    the chelange is created and done

---
+++ js/chalange.js
@@ -0,0 +1,15 @@
+let theTitle = "elzero",
+  theDesc = "elzero web school",
+  theDate = "25/10";
+
+let theCard = `
+<div>
+    <h3> ${theTitle} </h3>
+    <p> ${theDesc} </p>
+    <span> ${theDate}</p>
+    </div>
+`;
+document.write(theCard);
+document.write(theCard);
+document.write(theCard);
+document.write(theCard);
